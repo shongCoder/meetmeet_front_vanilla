f887aba9d929d131de984862dc09d36cf0f132cd
PARK SO YOUNG <itnj.parksy@gmail.com>
2025-03-24 13:38:37 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { setupCounter } from './counter.js';
+import './input.css';
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <div class="bg-meet_pink text-meet_white font-bold p-4 rounded-3xl text-center">
+    🎉 Tailwind 적용 완료!
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        meet_pink: "#FA1155",
+        meet_pink_hover: "#bf1651",
+        meet_menu: "#666666",
+        meet_text: "#7D8A9F",
+        meet_black: "#1C1E22",
+        meet_gray: "#E8E8E8",
+        meet_light_gray: "#FBF9F9",
+        meet_sodomy: "#2E3C52",
+        meet_blue: "#4D67EB",
+        meet_white: "#FFFFFF",
+      },
+      fontFamily: {
+        sans: ["Pretendard", "sans-serif"],
+      },
+      screens: {
+        md: "640px",
+        lg: "1440px",
+      },
+    },
+  },
+  plugins: [],
+};
