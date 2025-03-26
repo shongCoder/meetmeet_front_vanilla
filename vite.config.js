90d392db7e57872d8e7e6855e28fb531bc952786
PARK SO YOUNG <itnj.parksy@gmail.com>
2025-03-26 10:37:21 +0900

    빌드 메뉴 구조 설정

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
+import { resolve } from 'path'; // ✅ 이거 꼭 필요함
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/meetmeet_front_vanilla/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        influencer: resolve(__dirname, 'influencer.html'),
+      },
+    },
+  },
 });
